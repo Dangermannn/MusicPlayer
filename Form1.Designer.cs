8154d7f97128d2024439c85b522d60888d82ab8d
Dangermannn <dangermanplyt@gmail.com>
2020-07-18 18:19:51 +0200

    After pressing main category the subbuttons slide down

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,22 +35,25 @@
             this.btnFavouritePlaylists = new System.Windows.Forms.Button();
             this.panelLogo = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnMedia = new System.Windows.Forms.Button();
-            this.panelMediaSubMenu = new System.Windows.Forms.Panel();
-            this.btnOpenFiles = new System.Windows.Forms.Button();
-            this.btnOpenFolder = new System.Windows.Forms.Button();
-            this.btnOpenedMusic = new System.Windows.Forms.Button();
-            this.btnPlaylists = new System.Windows.Forms.Button();
             this.panelPlaylistsSubMenu = new System.Windows.Forms.Panel();
-            this.btnCreatePlaylist = new System.Windows.Forms.Button();
             this.btnShowAllPlaylists = new System.Windows.Forms.Button();
+            this.btnCreatePlaylist = new System.Windows.Forms.Button();
+            this.btnPlaylists = new System.Windows.Forms.Button();
+            this.panelMediaSubMenu = new System.Windows.Forms.Panel();
+            this.btnOpenedMusic = new System.Windows.Forms.Button();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            this.btnOpenFiles = new System.Windows.Forms.Button();
+            this.btnMedia = new System.Windows.Forms.Button();
             this.panelMenu = new System.Windows.Forms.Panel();
             this.panelMainDesktop = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panelCard.SuspendLayout();
+            this.panelLogo.SuspendLayout();
             this.panel1.SuspendLayout();
-            this.panelMediaSubMenu.SuspendLayout();
             this.panelPlaylistsSubMenu.SuspendLayout();
+            this.panelMediaSubMenu.SuspendLayout();
             this.panelMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnAbout
@@ -66,6 +69,7 @@
             this.btnAbout.TabIndex = 3;
             this.btnAbout.Text = "About";
             this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
             // 
             // panelCard
             // 
@@ -79,6 +83,7 @@
             // 
             // lblTitle
             // 
+            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Monospac821 BT", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitle.ForeColor = System.Drawing.Color.Cornsilk;
@@ -114,6 +119,8 @@
             // panelLogo
             // 
             this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panelLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panelLogo.Controls.Add(this.pictureBox1);
             this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelLogo.Location = new System.Drawing.Point(0, 0);
             this.panelLogo.Name = "panelLogo";
@@ -132,88 +139,6 @@
             this.panel1.Size = new System.Drawing.Size(200, 344);
             this.panel1.TabIndex = 2;
             // 
-            // btnMedia
-            // 
-            this.btnMedia.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnMedia.FlatAppearance.BorderSize = 0;
-            this.btnMedia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMedia.Font = new System.Drawing.Font("Monospac821 BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMedia.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnMedia.Location = new System.Drawing.Point(0, 0);
-            this.btnMedia.Name = "btnMedia";
-            this.btnMedia.Size = new System.Drawing.Size(200, 60);
-            this.btnMedia.TabIndex = 0;
-            this.btnMedia.Text = "Media";
-            this.btnMedia.UseVisualStyleBackColor = true;
-            // 
-            // panelMediaSubMenu
-            // 
-            this.panelMediaSubMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(65)))));
-            this.panelMediaSubMenu.Controls.Add(this.btnOpenedMusic);
-            this.panelMediaSubMenu.Controls.Add(this.btnOpenFolder);
-            this.panelMediaSubMenu.Controls.Add(this.btnOpenFiles);
-            this.panelMediaSubMenu.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelMediaSubMenu.Location = new System.Drawing.Point(0, 60);
-            this.panelMediaSubMenu.Name = "panelMediaSubMenu";
-            this.panelMediaSubMenu.Size = new System.Drawing.Size(200, 115);
-            this.panelMediaSubMenu.TabIndex = 1;
-            // 
-            // btnOpenFiles
-            // 
-            this.btnOpenFiles.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnOpenFiles.FlatAppearance.BorderSize = 0;
-            this.btnOpenFiles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnOpenFiles.Font = new System.Drawing.Font("Monospac821 BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOpenFiles.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.btnOpenFiles.Location = new System.Drawing.Point(0, 0);
-            this.btnOpenFiles.Name = "btnOpenFiles";
-            this.btnOpenFiles.Size = new System.Drawing.Size(200, 35);
-            this.btnOpenFiles.TabIndex = 0;
-            this.btnOpenFiles.Text = "Open files";
-            this.btnOpenFiles.UseVisualStyleBackColor = true;
-            // 
-            // btnOpenFolder
-            // 
-            this.btnOpenFolder.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnOpenFolder.FlatAppearance.BorderSize = 0;
-            this.btnOpenFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnOpenFolder.Font = new System.Drawing.Font("Monospac821 BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOpenFolder.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.btnOpenFolder.Location = new System.Drawing.Point(0, 35);
-            this.btnOpenFolder.Name = "btnOpenFolder";
-            this.btnOpenFolder.Size = new System.Drawing.Size(200, 35);
-            this.btnOpenFolder.TabIndex = 1;
-            this.btnOpenFolder.Text = "Open folder";
-            this.btnOpenFolder.UseVisualStyleBackColor = true;
-            // 
-            // btnOpenedMusic
-            // 
-            this.btnOpenedMusic.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnOpenedMusic.FlatAppearance.BorderSize = 0;
-            this.btnOpenedMusic.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnOpenedMusic.Font = new System.Drawing.Font("Monospac821 BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOpenedMusic.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.btnOpenedMusic.Location = new System.Drawing.Point(0, 70);
-            this.btnOpenedMusic.Name = "btnOpenedMusic";
-            this.btnOpenedMusic.Size = new System.Drawing.Size(200, 35);
-            this.btnOpenedMusic.TabIndex = 2;
-            this.btnOpenedMusic.Text = "Opened music";
-            this.btnOpenedMusic.UseVisualStyleBackColor = true;
-            // 
-            // btnPlaylists
-            // 
-            this.btnPlaylists.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnPlaylists.FlatAppearance.BorderSize = 0;
-            this.btnPlaylists.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnPlaylists.Font = new System.Drawing.Font("Monospac821 BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPlaylists.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnPlaylists.Location = new System.Drawing.Point(0, 175);
-            this.btnPlaylists.Name = "btnPlaylists";
-            this.btnPlaylists.Size = new System.Drawing.Size(200, 60);
-            this.btnPlaylists.TabIndex = 2;
-            this.btnPlaylists.Text = "Playlists";
-            this.btnPlaylists.UseVisualStyleBackColor = true;
-            // 
             // panelPlaylistsSubMenu
             // 
             this.panelPlaylistsSubMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(65)))));
@@ -225,6 +150,20 @@
             this.panelPlaylistsSubMenu.Name = "panelPlaylistsSubMenu";
             this.panelPlaylistsSubMenu.Size = new System.Drawing.Size(200, 113);
             this.panelPlaylistsSubMenu.TabIndex = 3;
+            // 
+            // btnShowAllPlaylists
+            // 
+            this.btnShowAllPlaylists.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnShowAllPlaylists.FlatAppearance.BorderSize = 0;
+            this.btnShowAllPlaylists.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnShowAllPlaylists.Font = new System.Drawing.Font("Monospac821 BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShowAllPlaylists.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.btnShowAllPlaylists.Location = new System.Drawing.Point(0, 70);
+            this.btnShowAllPlaylists.Name = "btnShowAllPlaylists";
+            this.btnShowAllPlaylists.Size = new System.Drawing.Size(200, 35);
+            this.btnShowAllPlaylists.TabIndex = 1;
+            this.btnShowAllPlaylists.Text = "Show all playlists";
+            this.btnShowAllPlaylists.UseVisualStyleBackColor = true;
             // 
             // btnCreatePlaylist
             // 
@@ -240,19 +179,89 @@
             this.btnCreatePlaylist.Text = "Create playlist";
             this.btnCreatePlaylist.UseVisualStyleBackColor = true;
             // 
-            // btnShowAllPlaylists
+            // btnPlaylists
             // 
-            this.btnShowAllPlaylists.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnShowAllPlaylists.FlatAppearance.BorderSize = 0;
-            this.btnShowAllPlaylists.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnShowAllPlaylists.Font = new System.Drawing.Font("Monospac821 BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnShowAllPlaylists.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.btnShowAllPlaylists.Location = new System.Drawing.Point(0, 70);
-            this.btnShowAllPlaylists.Name = "btnShowAllPlaylists";
-            this.btnShowAllPlaylists.Size = new System.Drawing.Size(200, 35);
-            this.btnShowAllPlaylists.TabIndex = 1;
-            this.btnShowAllPlaylists.Text = "Show all playlists";
-            this.btnShowAllPlaylists.UseVisualStyleBackColor = true;
+            this.btnPlaylists.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPlaylists.FlatAppearance.BorderSize = 0;
+            this.btnPlaylists.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPlaylists.Font = new System.Drawing.Font("Monospac821 BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlaylists.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnPlaylists.Location = new System.Drawing.Point(0, 175);
+            this.btnPlaylists.Name = "btnPlaylists";
+            this.btnPlaylists.Size = new System.Drawing.Size(200, 60);
+            this.btnPlaylists.TabIndex = 2;
+            this.btnPlaylists.Text = "Playlists";
+            this.btnPlaylists.UseVisualStyleBackColor = true;
+            this.btnPlaylists.Click += new System.EventHandler(this.btnPlaylists_Click);
+            // 
+            // panelMediaSubMenu
+            // 
+            this.panelMediaSubMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(65)))));
+            this.panelMediaSubMenu.Controls.Add(this.btnOpenedMusic);
+            this.panelMediaSubMenu.Controls.Add(this.btnOpenFolder);
+            this.panelMediaSubMenu.Controls.Add(this.btnOpenFiles);
+            this.panelMediaSubMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelMediaSubMenu.Location = new System.Drawing.Point(0, 60);
+            this.panelMediaSubMenu.Name = "panelMediaSubMenu";
+            this.panelMediaSubMenu.Size = new System.Drawing.Size(200, 115);
+            this.panelMediaSubMenu.TabIndex = 1;
+            // 
+            // btnOpenedMusic
+            // 
+            this.btnOpenedMusic.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnOpenedMusic.FlatAppearance.BorderSize = 0;
+            this.btnOpenedMusic.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpenedMusic.Font = new System.Drawing.Font("Monospac821 BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenedMusic.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.btnOpenedMusic.Location = new System.Drawing.Point(0, 70);
+            this.btnOpenedMusic.Name = "btnOpenedMusic";
+            this.btnOpenedMusic.Size = new System.Drawing.Size(200, 35);
+            this.btnOpenedMusic.TabIndex = 2;
+            this.btnOpenedMusic.Text = "Opened music";
+            this.btnOpenedMusic.UseVisualStyleBackColor = true;
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnOpenFolder.FlatAppearance.BorderSize = 0;
+            this.btnOpenFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpenFolder.Font = new System.Drawing.Font("Monospac821 BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenFolder.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.btnOpenFolder.Location = new System.Drawing.Point(0, 35);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(200, 35);
+            this.btnOpenFolder.TabIndex = 1;
+            this.btnOpenFolder.Text = "Open folder";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            // 
+            // btnOpenFiles
+            // 
+            this.btnOpenFiles.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnOpenFiles.FlatAppearance.BorderSize = 0;
+            this.btnOpenFiles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpenFiles.Font = new System.Drawing.Font("Monospac821 BT", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenFiles.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.btnOpenFiles.Location = new System.Drawing.Point(0, 0);
+            this.btnOpenFiles.Name = "btnOpenFiles";
+            this.btnOpenFiles.Size = new System.Drawing.Size(200, 35);
+            this.btnOpenFiles.TabIndex = 0;
+            this.btnOpenFiles.Text = "Open files";
+            this.btnOpenFiles.UseVisualStyleBackColor = true;
+            // 
+            // btnMedia
+            // 
+            this.btnMedia.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMedia.FlatAppearance.BorderSize = 0;
+            this.btnMedia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMedia.Font = new System.Drawing.Font("Monospac821 BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMedia.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnMedia.Location = new System.Drawing.Point(0, 0);
+            this.btnMedia.Name = "btnMedia";
+            this.btnMedia.Size = new System.Drawing.Size(200, 60);
+            this.btnMedia.TabIndex = 0;
+            this.btnMedia.Text = "Media";
+            this.btnMedia.UseVisualStyleBackColor = true;
+            this.btnMedia.Click += new System.EventHandler(this.btnMedia_Click);
             // 
             // panelMenu
             // 
@@ -274,6 +283,16 @@
             this.panelMainDesktop.Size = new System.Drawing.Size(628, 344);
             this.panelMainDesktop.TabIndex = 3;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MusicPlayer.Properties.Resources.cat_no_bg;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(41, 37);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -287,10 +306,12 @@
             this.Text = "Form1";
             this.panelCard.ResumeLayout(false);
             this.panelCard.PerformLayout();
+            this.panelLogo.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
-            this.panelMediaSubMenu.ResumeLayout(false);
             this.panelPlaylistsSubMenu.ResumeLayout(false);
+            this.panelMediaSubMenu.ResumeLayout(false);
             this.panelMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -314,6 +335,7 @@
         private System.Windows.Forms.Button btnMedia;
         private System.Windows.Forms.Panel panelMenu;
         private System.Windows.Forms.Panel panelMainDesktop;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,101 @@ namespace MusicPlayer
 {
     public partial class Form1 : Form
     {
+        public static Color PrimaryColor { get; set; }
+        public static Color SecondaryColor { get; set; }
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
         public Form1()
         {
             InitializeComponent();
+            hideSubMenuAtStart();
+            random = new Random();
+        }
+        private void hideSubMenuAtStart()
+        {
+            panelPlaylistsSubMenu.Visible = false;
+            panelMediaSubMenu.Visible = false;
+        }
+
+        private void hideSubMenu()
+        {
+            if (panelMediaSubMenu.Visible)
+                panelMediaSubMenu.Visible = false;
+            if (panelPlaylistsSubMenu.Visible)
+                panelPlaylistsSubMenu.Visible = false;
+        }
+
+        private void showSubMenu(Panel subMenu)
+        {
+            if (!subMenu.Visible)
+            {
+                hideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+                subMenu.Visible = false;
+        }
+
+        private Color SelectThemeColor()
+        {
+            int index = random.Next(ThemeColor.ColorList.Count);
+            while (tempIndex == index)
+                index = random.Next(ThemeColor.ColorList.Count);
+            tempIndex = index;
+            string color = ThemeColor.ColorList[index];
+            return ColorTranslator.FromHtml(color);
+        }
+
+        private void ActivateButton(object btnSender)
+        {
+            if(btnSender != null)
+            {
+                if(currentButton != (Button)btnSender)
+                {
+                    DisableButtons();
+                    Color color = SelectThemeColor();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    panelCard.BackColor = color;
+                    panelLogo.BackColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    ThemeColor.PrimaryColor = color;
+                    ThemeColor.SecondaryColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                }
+            }
+        }
+
+        private void DisableButtons()
+        {
+            foreach(Control btn in panel1.Controls)
+            {
+                if(btn.GetType() == typeof(Button))
+                {
+                    Console.WriteLine("DISABLING");
+                    btn.BackColor = Color.FromArgb(25, 25, 65);
+                    btn.ForeColor = Color.Gainsboro;
+                    btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+            showSubMenu(panelMediaSubMenu);
+        }
+
+        private void btnPlaylists_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+            showSubMenu(panelPlaylistsSubMenu);
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
         }
     }
 }
